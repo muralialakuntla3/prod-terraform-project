4feed56f15779d284823ca4d5a45ba1c0b80c9f2
muralialakuntla3 <muralialakuntla3@gmail.com>
2025-06-25 19:33:52 +0530

    updated provider

--- provider.tf
+++ provider.tf
@@ -5,4 +5,10 @@ terraform {
       version = ">= 5.0"
     }
   }
+
+  backend "s3" {}  # <-- REQUIRED placeholder for Terragrunt's remote_state to inject config
+}
+
+provider "aws" {
+  region = us-west-1  # Or hardcode a default if you prefer
 }

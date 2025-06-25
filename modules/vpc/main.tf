331691fe20cc839438292aabe724a59284cc34c6
muralialakuntla3 <muralialakuntla3@gmail.com>
2025-06-25 20:54:13 +0530

    updated vpc configuration

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {}
 }
 
-resource "aws_vpc" "this" {
+resource "aws_vpc" "my-vpc" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
   enable_dns_hostnames = true
